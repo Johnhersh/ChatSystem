5dcc48e161f8ef9989c255e88eed24029597ef05
JohnHersh <johnher@gmail.com>
2019-06-22 22:41:15 -0700

    Initial check-in of module ChatSystem_v3

---
+++ Areas/Identity/Data/ChatSystem_v3IdentityDbContext.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatSystem_v3.Areas.Identity.Data
+{
+    public class ChatSystem_v3IdentityDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ChatSystem_v3IdentityDbContext(DbContextOptions<ChatSystem_v3IdentityDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
