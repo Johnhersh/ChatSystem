63f1bfaca29fd749f538f15ae0955675c35c7f19
JohnHersh <johnher@gmail.com>
2019-06-23 20:56:09 -0700

    Fixed margins and added date

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -33,22 +33,35 @@
         return console.error(err.toString());
     });
 
+    function pad(n) {
+       return n<10 ? '0'+n : n;
+    }
+
     connection.on("ReceiveMessage", function (user, message, self) {
         var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
 
         var ScrollBox = document.getElementById("messagesList");
         var newMessage = document.createElement('div');
 
+        var currentDate = new Date();
+        var date = currentDate.getDate();
+        var month = currentDate.getMonth(); 
+        var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var time = currentDate.getHours() + ":" + currentDate.getMinutes();
+
+        var FullDate = monthNames[month] + " " + pad(date) + " " + time;
+
+
         msg = "<h6 align='left'>" + msg + "</h>";
         console.log(self);
         if (self) {
             newMessage.className = ('list-group-item ml-auto text-right list-group-item-secondary');
-            newMessage.style = ('display: inline-block; max-width: 60%; right:20px; border-radius: 25px;');
+            newMessage.style = ('display: inline-block; max-width: 60%; right:20px; border-radius: 25px; margin-top:2px; margin-bottom:2px;');
             newMessage.innerHTML = msg;
         } else {
-            newMessage.className = ('list-group-item rounded-pill');
-            newMessage.style = ('display: inline-block; max-width: 60%');
-            newMessage.innerHTML = user+': '+msg;
+            newMessage.className = ('list-group-item');
+            newMessage.style = ('display: inline-block; max-width: 60%; border-radius: 25px; margin-top:2px; margin-bottom:2px;');
+            newMessage.innerHTML = '<h6 class="text-success">'+user+'  <small>'+FullDate+'</small>'+'</h6>'+msg;
         }
 
         document.getElementById("messagesList").appendChild(newMessage);
